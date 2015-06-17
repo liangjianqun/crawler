e1b6cc507de201a67f58e0f66027ce9aa3dbf06a
liangjianqun <lucky.qun@163.com>
2015-06-17 06:02:15 -0400

    Mod tools/txt.sh

--- tools/txt.sh
+++ tools/txt.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
-sed '1,6d' /usr/local/
+for i in {1..11};do
+    echo "process /usr/local/tomcat/webapps/ROOT/txt/0/${i}/*.txt"
+#    sed -i '1,6d' /usr/local/tomcat/webapps/ROOT/txt/0/${i}/*.txt
+    sed  -i "s/&nbsp;/ /g" /usr/local/tomcat/webapps/ROOT/txt/0/${i}/*.txt
+    sed  -i "s/&ldquo;/\"/g" /usr/local/tomcat/webapps/ROOT/txt/0/${i}/*.txt
+    sed  -i "s/&rdquo;/\"/g" /usr/local/tomcat/webapps/ROOT/txt/0/${i}/*.txt
+    sed  -i "s/&hellip;/.../g" /usr/local/tomcat/webapps/ROOT/txt/0/${i}/*.txt
+    sed  -i "s/&mdash;/--/g" /usr/local/tomcat/webapps/ROOT/txt/0/${i}/*.txt
+    sed  -i "s/&middot;/./g" /usr/local/tomcat/webapps/ROOT/txt/0/${i}/*.txt
+    sed  -i "s/&lsquo;/'/g" /usr/local/tomcat/webapps/ROOT/txt/0/${i}/*.txt
+    sed  -i "s/&rsquo;/'/g" /usr/local/tomcat/webapps/ROOT/txt/0/${i}/*.txt
+done
