266c947fb48061be76aab4c8dc3ed69abff6c001
liangjianqun <lucky.qun@163.com>
2015-06-19 04:28:54 -0400

    Mod build.sh

--- build.sh
+++ build.sh
@@ -11,4 +11,5 @@ cd ../
 echo -n "classpath=" > run/run.sh
 for f in `ls run/crawler/lib`;do echo -n ":./crawler/lib/${f}";done>> run/run.sh
 echo "">>run/run.sh
+
 echo "exec java -Xms512m -Xmx512m -ea -cp \${classpath} crawler.core.Job $@ 1>>std.out 2>>std.err &" >> run/run.sh 
