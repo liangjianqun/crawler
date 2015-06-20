b577c1611e2c1ca75dd45a255842f32c6b434a86
liangjianqun <www.kernel.cn@gmail.com>
2015-06-20 10:57:36 +0800

    Mod txt.sh

--- tools/txt.sh
+++ tools/txt.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
-for i in {1..11};do
-    echo "process /usr/local/tomcat/webapps/ROOT/txt/0/${i}/*.txt"
-#    sed -i '1,6d' /usr/local/tomcat/webapps/ROOT/txt/0/${i}/*.txt
-    sed  -i "s/&nbsp;/ /g" /usr/local/tomcat/webapps/ROOT/txt/0/${i}/*.txt
-    sed  -i "s/&ldquo;/\"/g" /usr/local/tomcat/webapps/ROOT/txt/0/${i}/*.txt
-    sed  -i "s/&rdquo;/\"/g" /usr/local/tomcat/webapps/ROOT/txt/0/${i}/*.txt
-    sed  -i "s/&hellip;/.../g" /usr/local/tomcat/webapps/ROOT/txt/0/${i}/*.txt
-    sed  -i "s/&mdash;/--/g" /usr/local/tomcat/webapps/ROOT/txt/0/${i}/*.txt
-    sed  -i "s/&middot;/./g" /usr/local/tomcat/webapps/ROOT/txt/0/${i}/*.txt
-    sed  -i "s/&lsquo;/'/g" /usr/local/tomcat/webapps/ROOT/txt/0/${i}/*.txt
-    sed  -i "s/&rsquo;/'/g" /usr/local/tomcat/webapps/ROOT/txt/0/${i}/*.txt
+for i in {1000..2011};do
+    ((d=i/1000))
+    echo "process /usr/local/tomcat/webapps/ROOT/txt/${d}/${i}/*.txt"
+    sed -i '1,6d' /usr/local/tomcat/webapps/ROOT/txt/${d}/${i}/*.txt
+    sed  -i "s/&nbsp;/ /g" /usr/local/tomcat/webapps/ROOT/txt/${d}/${i}/*.txt
+    sed  -i "s/&ldquo;/\"/g" /usr/local/tomcat/webapps/ROOT/txt/${d}/${i}/*.txt
+    sed  -i "s/&rdquo;/\"/g" /usr/local/tomcat/webapps/ROOT/txt/${d}/${i}/*.txt
+    sed  -i "s/&hellip;/.../g" /usr/local/tomcat/webapps/ROOT/txt/${d}/${i}/*.txt
+    sed  -i "s/&mdash;/--/g" /usr/local/tomcat/webapps/ROOT/txt/${d}/${i}/*.txt
+    sed  -i "s/&middot;/./g" /usr/local/tomcat/webapps/ROOT/txt/${d}/${i}/*.txt
+    sed  -i "s/&lsquo;/'/g" /usr/local/tomcat/webapps/ROOT/txt/${d}/${i}/*.txt
+    sed  -i "s/&rsquo;/'/g" /usr/local/tomcat/webapps/ROOT/txt/${d}/${i}/*.txt
 done
