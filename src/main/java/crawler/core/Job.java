f29d460a30f66fe42d3037273abf52d5047fddf2
liangjianqun <lucky.qun@163.com>
2015-06-30 16:21:58 +0800

    Add
    log4j

--- src/main/java/crawler/core/Job.java
+++ src/main/java/crawler/core/Job.java
@@ -9,6 +9,7 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.htmlparser.tags.Div;
 
 import crawler.common.Api;
@@ -21,6 +22,7 @@ import crawler.parser.ArticleParser;
 import crawler.parser.FastParser;
 
 public class Job {
+	private static Logger logger = Logger.getLogger(Job.class);
 	private String articleUrl_ = null;
 	private Article article_ = null;
 	private Chapter chapter_ = null;
@@ -30,29 +32,30 @@ public class Job {
 	private boolean dropArticle_ = false;
 	private boolean dropChapter_ = false;
 	private String baseUrl_ = "http://www.kaixinwx.com";
-	
+
 	public Job(String articleUrl) {
 		articleUrl_ = articleUrl;
 	}
-	
+
 	public int ParseArticle() {
-		System.out.println("Try to fetch " + articleUrl_);
+		logger.info("Try to fetch " + articleUrl_);
 		Crawler crawler = new Crawler();
-		byte[] html = crawler.FetchByGet(articleUrl_, Crawler.DefaultProperties(), Api.kFetchRetry);
-  
+		byte[] html = crawler.FetchByGet(articleUrl_,
+				Crawler.DefaultProperties(), Api.kFetchRetry);
+
 		if (html == null) {
 			return -1;
 		}
 		List<String> toReplace = new ArrayList<String>();
 		toReplace.add("开心");
 		article_ = ArticleParser.Parse(new String(html), toReplace);
-		
+
 		return 0;
 	}
-	
+
 	public static String CoverPath(int articleNo, int imgflag, boolean createDir) {
 		String file = Api.kDeployRoot + Api.kDeployCover;
-		file = file + (articleNo / Api.kArticleHashNum) + "/"; 
+		file = file + (articleNo / Api.kArticleHashNum) + "/";
 		file = file + articleNo + "/";
 		if (createDir) {
 			Utils.Makedir(file);
@@ -60,21 +63,21 @@ public class Job {
 		file = file + articleNo + Article.ImgSuffix(imgflag);
 		return file;
 	}
-	
+
 	public static int SaveCoverToDisk(byte[] img, int articleNo, int imgflag) {
 		int result = 0;
 		String file = CoverPath(articleNo, imgflag, true);
-		
+
 		try {
 			Utils.WriteFile(img, file);
 		} catch (IOException e) {
 			result = -1;
 			e.printStackTrace();
 		}
-		
+
 		return result;
 	}
-	
+
 	public int SaveArticleToDb() {
 		int result = 0;
 		Connection con = DbUtils.GetConnection();
@@ -84,7 +87,8 @@ public class Job {
 			if (pstmt.execute(article_.Sql() + " RETURNING articleno;")) {
 				ResultSet rs = pstmt.getResultSet();
 				if (rs.next()) {
-					lastArticleNo_ = Integer.parseInt(rs.getString("articleno"));
+					lastArticleNo_ = Integer
+							.parseInt(rs.getString("articleno"));
 					article_.setArticleno(lastArticleNo_);
 				}
 			}
@@ -101,7 +105,7 @@ public class Job {
 		}
 		return result;
 	}
-	
+
 	public int ProcessChapter() {
 		List<Pair<String, String>> list = article_.getChapterList();
 		for (int i = 0; i < list.size(); ++i) {
@@ -109,54 +113,72 @@ public class Job {
 			chapter_.setArticleno(article_.getArticleno());
 			chapter_.setArticlename(article_.getArticlename());
 			chapter_.setChaptername(list.get(i).first());
-			
+
 			String url = "";
 			if (list.get(i).second().indexOf("http") < 0) {
 				url = baseUrl_;
 			}
 			url += list.get(i).second();
-			
-            System.out.println("LastChapterNo " + lastChapterNo_ + " Begin to ProcessChapter " + url);    
-			byte[] html = crawler_.FetchByGet(url, Crawler.DefaultProperties(), Api.kFetchRetry);
+
+			logger.info("LastChapterNo " + lastChapterNo_
+					+ " Begin to ProcessChapter " + url);
+			byte[] html = crawler_.FetchByGet(url, Crawler.DefaultProperties(),
+					Api.kFetchRetry);
 			if (html == null) {
-				System.err.println("FATAL failed to fetch url " + url + " " + 
-								    ChapterPath(chapter_.getArticleno(), chapter_.getChapterno(), false));
+				logger.error("FATAL failed to fetch url "
+						+ url
+						+ " "
+						+ ChapterPath(chapter_.getArticleno(),
+								chapter_.getChapterno(), false));
 				continue;
 			}
 			String txt = ParseChapter(new String(html));
 			if (txt == null) {
-				System.err.println("FATAL failed to ParseChapter url " + url + " " +
-									ChapterPath(chapter_.getArticleno(), chapter_.getChapterno(), false));
+				logger.error("FATAL failed to ParseChapter url "
+						+ url
+						+ " "
+						+ ChapterPath(chapter_.getArticleno(),
+								chapter_.getChapterno(), false));
 				continue;
 			}
 			chapter_.setSize(txt.length());
 			article_.setSize(article_.getSize() + txt.length());
 			if (SaveChapterToDb(chapter_) != 0) {
-				System.err.println("FATAL failed to SaveChapterToDb url " + url + " " +
-									ChapterPath(chapter_.getArticleno(), chapter_.getChapterno(), false));
+				logger.error("FATAL failed to SaveChapterToDb url "
+						+ url
+						+ " "
+						+ ChapterPath(chapter_.getArticleno(),
+								chapter_.getChapterno(), false));
 				continue;
 			}
-			if (SaveChapterToDisk(txt, chapter_.getArticleno(), chapter_.getChapterno()) != 0) {
-				System.err.println("FATAL failed to SaveChapterToDisk url " + url + " " +
-									ChapterPath(chapter_.getArticleno(), chapter_.getChapterno(), false));
+			if (SaveChapterToDisk(txt, chapter_.getArticleno(),
+					chapter_.getChapterno()) != 0) {
+				logger.error("FATAL failed to SaveChapterToDisk url "
+						+ url
+						+ " "
+						+ ChapterPath(chapter_.getArticleno(),
+								chapter_.getChapterno(), false));
 				continue;
 			}
-			if ((i % 100 == 0 || (i == list.size() - 1)) &&
-				UpdateLastChapter(chapter_.getChapterno(), list.get(i).first()) != 0) {
-				System.err.println("FATAL failed to UpdateLastChapter " + article_.getArticlename());
+			if ((i % 100 == 0 || (i == list.size() - 1))
+					&& UpdateLastChapter(chapter_.getChapterno(), list.get(i)
+							.first()) != 0) {
+				logger.error("FATAL failed to UpdateLastChapter "
+						+ article_.getArticlename());
 			}
 		}
 
 		return 0;
 	}
-	
+
 	private int UpdateLastChapter(int lastNo, String lastChapter) {
 		int result = 0;
 		Connection con = DbUtils.GetConnection();
 		Statement pstmt = null;
-		String sql = "UPDATE t_article SET lastchapterno = " + lastNo + 
-				",lastchapter = '" + lastChapter + "'" + ", size = " + article_.getSize() +
-				" WHERE articleno = " + article_.getArticleno();
+		String sql = "UPDATE t_article SET lastchapterno = " + lastNo
+				+ ",lastchapter = '" + lastChapter + "'" + ", size = "
+				+ article_.getSize() + " WHERE articleno = "
+				+ article_.getArticleno();
 		try {
 			pstmt = con.createStatement();
 			pstmt.execute(sql);
@@ -173,18 +195,20 @@ public class Job {
 		}
 		return result;
 	}
-	
+
 	public String ParseChapter(String html) {
-        List<Div> authors=FastParser.parseTags(html, Div.class, "class", "mainContenr"); 
+		List<Div> authors = FastParser.parseTags(html, Div.class, "class",
+				"mainContenr");
 		for (Div div : authors) {
 			return div.getStringText();
 		}
 		return null;
 	}
-	
-	public static String ChapterPath(int articleNo, int chapterNo, boolean createDir) {
+
+	public static String ChapterPath(int articleNo, int chapterNo,
+			boolean createDir) {
 		String file = Api.kDeployRoot + Api.kDeployTxt;
-		file = file + (articleNo / Api.kArticleHashNum) + "/"; 
+		file = file + (articleNo / Api.kArticleHashNum) + "/";
 		file = file + articleNo + "/";
 		if (createDir) {
 			Utils.Makedir(file);
@@ -192,21 +216,21 @@ public class Job {
 		file = file + chapterNo + ".txt";
 		return file;
 	}
-	
+
 	private int SaveChapterToDisk(String txt, int articleNo, int chapterNo) {
 		int result = 0;
 		String file = ChapterPath(articleNo, chapterNo, true);
-		
+
 		try {
 			Utils.WriteFile(txt, file);
 		} catch (IOException e) {
 			result = -1;
 			e.printStackTrace();
 		}
-		
+
 		return result;
 	}
-	
+
 	public int SaveChapterToDb(Chapter chapter) {
 		int result = 0;
 		Connection con = DbUtils.GetConnection();
@@ -216,7 +240,8 @@ public class Job {
 			if (pstmt.execute(chapter.Sql() + " RETURNING chapterno;")) {
 				ResultSet rs = pstmt.getResultSet();
 				if (rs.next()) {
-					lastChapterNo_ = Integer.parseInt(rs.getString("chapterno"));
+					lastChapterNo_ = Integer
+							.parseInt(rs.getString("chapterno"));
 					chapter.setChapterno(lastChapterNo_);
 				}
 			}
@@ -233,10 +258,11 @@ public class Job {
 		}
 		return result;
 	}
-	
+
 	public static boolean AlreadyHasArticle(String name) {
 		Connection con = DbUtils.GetConnection();
-		String sql = "select * from t_article where articlename = '" + name+ "'";
+		String sql = "select * from t_article where articlename = '" + name
+				+ "'";
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 		try {
@@ -257,22 +283,24 @@ public class Job {
 		}
 		return false;
 	}
-	
+
 	public int ProcessArticle() {
 		int result = 0;
-        System.out.println("LastArticleNo " + lastArticleNo_ +" Begin to ProcessArticle " + articleUrl_);    
+		logger.info("LastArticleNo " + lastArticleNo_
+				+ " Begin to ProcessArticle " + articleUrl_);
 		if (ParseArticle() != 0) {
-			System.err.println("FATAL failed to ParseArticle " + articleUrl_);
+			logger.error("FATAL failed to ParseArticle " + articleUrl_);
 			result = -1;
 			return result;
 		}
 		if (AlreadyHasArticle(article_.getArticlename())) {
-			System.out.println("AlreadyHasArticle " + articleUrl_ + " " + article_.getArticlename());  
+			logger.info("AlreadyHasArticle " + articleUrl_ + " "
+					+ article_.getArticlename());
 			return -1;
 		}
 
 		if (SaveArticleToDb() != 0) {
-			System.err.println("FATAL failed to SaveArticleToDb url " + articleUrl_);
+			logger.error("FATAL failed to SaveArticleToDb url " + articleUrl_);
 			return -1;
 		}
 		String url = "";
@@ -280,32 +308,41 @@ public class Job {
 			url = baseUrl_;
 		}
 		url += article_.getNovelCover();
-		byte[] html = crawler_.FetchByGet(url, Crawler.DefaultProperties(), Api.kFetchRetry);
+		byte[] html = crawler_.FetchByGet(url, Crawler.DefaultProperties(),
+				Api.kFetchRetry);
 		if (html == null) {
-			System.err.println("FATAL failed to fetch url " + url + " " +
-							    CoverPath(article_.getArticleno(), article_.getImgflag(), false));
+			logger.error("FATAL failed to fetch url "
+					+ url
+					+ " "
+					+ CoverPath(article_.getArticleno(), article_.getImgflag(),
+							false));
 			dropArticle_ = true;
 			return -1;
 		}
-        if (SaveCoverToDisk(html, article_.getArticleno(), article_.getImgflag()) != 0) {
-            System.err.println("FATAL failed to SaveCoverToDisk url " + url + " " + 
-            					   CoverPath(article_.getArticleno(), article_.getImgflag(), false));
-            dropArticle_ = true;
-            return -1;
-        }
+		if (SaveCoverToDisk(html, article_.getArticleno(),
+				article_.getImgflag()) != 0) {
+			logger.error("FATAL failed to SaveCoverToDisk url "
+					+ url
+					+ " "
+					+ CoverPath(article_.getArticleno(), article_.getImgflag(),
+							false));
+			dropArticle_ = true;
+			return -1;
+		}
 
 		return result;
 	}
-	
+
 	private int DropArticle() {
 		int result = 0;
 		Connection con = DbUtils.GetConnection();
 		Statement pstmt = null;
-		String sql = "delete from t_article where articleno = " + article_.getArticleno();
-		System.err.println("Begin to Drop article " + sql);
+		String sql = "delete from t_article where articleno = "
+				+ article_.getArticleno();
+		logger.error("Begin to Drop article " + sql);
 		try {
 			pstmt = con.createStatement();
-			pstmt.execute(sql); 
+			pstmt.execute(sql);
 		} catch (SQLException e) {
 			result = -1;
 			e.printStackTrace();
@@ -319,29 +356,30 @@ public class Job {
 		}
 		return result;
 	}
-	
+
 	public int Process() {
 		if (ProcessArticle() != 0) {
 			if (dropArticle_) {
 				DropArticle();
 			}
-            return -1;
+			return -1;
 		}
 		return ProcessChapter();
 	}
-	
+
 	public static void main(String[] args) {
+		logger.info("Begin now");
 		int start = 1;
 		int stop = 50000;
 		if (args.length >= 1) {
 			start = Integer.parseInt(args[0]);
-		} 
+		}
 		if (args.length >= 2) {
 			stop = Integer.parseInt(args[1]);
 		}
 		for (int i = start; i <= stop; ++i) {
-			Job job = new Job("http://www.kaixinwx.com/book/" + i +".html");
-			System.out.println(job.Process());
+			//Job job = new Job("http://www.kaixinwx.com/book/" + i + ".html");
+			//logger.info(job.Process());
 		}
 	}
 }
