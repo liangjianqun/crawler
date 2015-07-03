acd3f084025db8cd2e84fbca3673b90eadac4508
root <root@vultr.guest>
2015-07-03 17:05:01 +0800

    Mod tools/txt.sh

--- tools/txt.sh
+++ tools/txt.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 
-for i in {1000..2011};do
+#for i in {1281..1566};do
+#for i in {1567..4885};do
+#for i in {7406..48648};do
+#for i in {4886..7405};do
+#for i in {48648..53125};do
+#for i in {53126..53126};do
+#for i in {53127..60646};do
+#for i in {60647..68165};do
+#for i in {68166..79300};do
+for i in {79301..79301};do
     ((d=i/1000))
     echo "process /usr/local/tomcat/webapps/ROOT/txt/${d}/${i}/*.txt"
-    sed -i '1,6d' /usr/local/tomcat/webapps/ROOT/txt/${d}/${i}/*.txt
+    #sed -i '1,6d' /usr/local/tomcat/webapps/ROOT/txt/${d}/${i}/*.txt
     sed  -i "s/&nbsp;/ /g" /usr/local/tomcat/webapps/ROOT/txt/${d}/${i}/*.txt
     sed  -i "s/&ldquo;/\"/g" /usr/local/tomcat/webapps/ROOT/txt/${d}/${i}/*.txt
     sed  -i "s/&rdquo;/\"/g" /usr/local/tomcat/webapps/ROOT/txt/${d}/${i}/*.txt
