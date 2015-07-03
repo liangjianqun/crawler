36fc2b9c250c29ee1aee21c2a902be4aff1757bc
root <root@vultr.guest>
2015-07-03 17:05:19 +0800

    MOd tools/warter.sh

--- tools/warter.sh
+++ tools/warter.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-for i in {0..53125};do
+#for i in {0..53125};do
 #for i in {0..53125};do
 #for i in {53126..60646};do
 #for i in {60647..68165};do
+for i in {68166..88000};do
     ((d=i/1000))
     echo "mogrify ${i}"
     mogrify -resize 120x150  -pointsize 20 -verbose -draw " fill white text 6,144 'jipinshu.com' fill white text 7,145 'jipinshu.com' " /usr/local/tomcat/webapps/ROOT/cover/${d}/${i}/${i}*
